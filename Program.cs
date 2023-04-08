eead333ac9242b993a1c5fe1c04aad00d65c4e3e
KseniaShaveko <kshaveko04@gmail.com>
2023-04-08 10:08:19 +1000

    код

--- Program.cs
+++ Program.cs
@@ -1,16 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            string[] numbers = input.Replace(".", ",").Split(' ');
+            double[] components = new double[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (double.TryParse(numbers[i], out double num))
+                {
+                    components[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка преобразования числа {numbers[i]} в тип double.");
+                    return;
+                }
+            }
+
+            double sumSquares = 0;
+
+            foreach (double component in components)
+            {
+                double value = component;
+                sumSquares += value * value;
+            }
+
+            double result = Math.Sqrt(sumSquares);
+            string res;
+
+            if (result % 1 == 0)
+            {
+                res = result.ToString("0.0").Replace(",", ".");
+            }
+            else
+            {
+                res = result.ToString().Replace(",", ".");
+            }
+
+            Console.WriteLine(res);
         }
     }
+
 }
